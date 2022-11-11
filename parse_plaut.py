898cdf0075293ff62aa6df5b0d206ee4906a516d
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2022-11-11 02:01:17 -0600

    laid out function

--- parse_plaut.py
+++ parse_plaut.py
@@ -1,3 +1,4 @@
+import xml.dom.minidom as md
 # Script to resolve antiquated line identification scheme in Plautus references.
 # Function should take in a file, read it (prob should use helper function),
 # and then RegEx find every <bibl> tag with an author subtag of "Plaut."
@@ -6,3 +7,18 @@
 # e.g. Perseus:abo:phi,0119,006:5:2:41 to Perseus:abo:phi,0119,006:912. Also
 # add a parenthetical with the actual line number to the citation to go from
 # 5, 2, 41 to 5, 2, 41 (912).
+
+# input file
+
+# parse file as xml
+
+# find all bibl tags
+
+# for each bibl tag find author subtag
+
+# check if value == to "Plaut."
+
+# if it is, get value of title tag and call helper function to calculate line number
+
+# then replace end of n attribute for perseus link (that is, the num:num:num) with the line number
+# and add parenthetical with line number to end of bibl text (that is: "num, num, num (num)")
